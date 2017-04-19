9fb623acf45af474630b3a60682954ce5e74fc2d
deformer <serg19-03@mail.ru>
2017-04-19 17:11:47 +0300

    add cart repository

--- app/repositories/cart.js
+++ app/repositories/cart.js
@@ -1,8 +1,65 @@
 /**
  * Created by sbelan on 4/19/2017.
  */
-
+const { CartConstructor } = require('../helpers/constructors');
 
 class CartRepository {
+  constructor(store) {
+    this.cartStore = store;
+  }
 
+  add(product, quantity, callback) {
+    const { price, _id } = product;
+    const index = this.indexOfProduct(_id);
+
+    this.cartStore.data.products_count += quantity;
+    this.cartStore.data.total_sum += quantity * price;
+    if (index === -1) {
+      this.cartStore.data.products.push({
+        id: _id,
+        quantity,
+        sum: quantity * price,
+      });
+      callback();
+      return;
+    }
+    const currentProduct = this.cartStore.data.products[index];
+    currentProduct.quantity += quantity;
+    currentProduct.sum += quantity * price;
+    callback();
+  }
+
+  remove(product, callback) {
+    const { price, _id } = product;
+    const index = this.indexOfProduct(_id);
+    if (index === -1) {
+      callback('Такого продукта нет в корзине');
+      return;
+    }
+    const currentProduct = this.cartStore.data.products[index];
+    this.cartStore.data.total_sum -= price;
+    this.cartStore.data.products_count -= 1;
+    if (currentProduct.quantity === 1) {
+      this.cartStore.data.products.splice(index, 1);
+    } else {
+      currentProduct.quantity -= 1;
+      currentProduct.sum -= price;
+    }
+    callback();
+  }
+
+  get(callback) {
+    const cart = this.cartStore;
+    if (cart) {
+      callback(cart);
+    } else {
+      callback(new CartConstructor([], 0));
+    }
+  }
+
+  indexOfProduct(id) {
+    return this.cartStore.data.products.map(item => item.id).indexOf(id);
+  }
 }
+
+module.exports = CartRepository;
--- app/services/cart.js
+++ app/services/cart.js
@@ -2,85 +2,67 @@
  * Created by sbelan on 4/19/2017.
  */
 const ProductRepository = require('../repositories/product');
+const CartRepository = require('../repositories/cart');
 const { ParamsError } = require('../helpers/errors');
 const { CartConstructor } = require('../helpers/constructors');
 const Product = new ProductRepository();
 
+
 exports.addProductToCart = (req, res) => {
   const product_id = req.body.product_id;
   const quantity = req.body.quantity;
-  Product.getById(product_id, (err, [productObj]) => {
+  Product.getById(product_id, (err, [product]) => {
     if (err) {
       res.sendStatus(500);
       console.error(err);
-    } else if (!productObj) {
+      return;
+    } if (!product) {
       res.status(400).json(new ParamsError([{
         message: "'product_id' doesn't exist in database",
         path: 'product_id',
         type: 'bad parameter',
       }]));
-    } else if (!req.session.cart) {
-      req.session.cart = new CartConstructor(productObj, quantity);
-      req.session.cartMaxAge = Date.now() + 5 * 60 * 1000;
+      return;
+    } if (!req.session.cart) {
+      req.session.cart = new CartConstructor(product, quantity);
+      req.session.cartMaxAge = Date.now() + (5 * 60 * 1000);
       res.sendStatus(200);
-    } else {
-      let cart = req.session.cart,
-        index = cart.data.products.map(item => item.id).indexOf(product_id);
-      if (index === -1) {
-        cart.data.products.push({ id: productObj._id, quantity, sum: quantity * productObj.price });
-        cart.data.products_count += quantity;
-        cart.data.total_sum += quantity * productObj.price;
-        res.sendStatus(200);
-      } else {
-        const currentProduct = cart.data.products[index];
-        currentProduct.quantity += quantity;
-        currentProduct.sum += quantity * productObj.price;
-        cart.data.products_count += quantity;
-        cart.data.total_sum += quantity * productObj.price;
-        res.sendStatus(200);
-      }
+      return;
     }
+    const cart = new CartRepository(req.session.cart);
+    cart.add(product, quantity, () => {
+      res.sendStatus(200);
+    });
   });
 };
 
 exports.deleteProductFromCart = (req, res) => {
   const product_id = req.params.product_id;
-  if (req.session.cart) {
-    Product.getById(product_id, (err, [productObj]) => {
-      if (err) {
-        res.sendStatus(500);
-        console.error(err);
-      } else if (!productObj) {
-        res.status(400).send('Такого продукта нет в системе');
-      } else {
-        let cart = req.session.cart,
-          index = cart.data.products.map(item => item.id).indexOf(product_id);
-        if (index === -1) {
-          res.status(400).send('В вашей корзине нет такого товара');
-        } else {
-          const currentProduct = cart.data.products[index];
-          cart.data.total_sum -= productObj.price;
-          cart.data.products_count--;
-          if (currentProduct.quantity === 1) {
-            cart.data.products.splice(index, 1);
-          } else {
-            currentProduct.quantity--;
-            currentProduct.sum -= productObj.price;
-          }
-          res.sendStatus(200);
-        }
-      }
-    });
-  } else {
+  if (!req.session.cart) {
     res.status(400).send('Корзина пуста, вы не можете удалить из нее товар');
+    return;
   }
+  Product.getById(product_id, (err, [product]) => {
+    if (err) {
+      res.sendStatus(500);
+      console.error(err);
+      return;
+    } if (!product) {
+      res.status(400).send('Такого продукта нет в системе');
+      return;
+    }
+    const cart = new CartRepository(req.session.cart);
+    cart.remove(product, (error) => {
+      if (error) {
+        res.send(error).status(404);
+      } else res.sendStatus(200);
+    });
+  });
 };
 
 exports.getAllProductsInCart = (req, res) => {
-  const cart = req.session.cart;
-  if (cart) {
-    res.status(200).json(cart);
-  } else {
-    res.status(200).json(new CartConstructor(null, 0));
-  }
+  const cart = new CartRepository(req.session.cart);
+  cart.get((data) => {
+    res.json(data).status(200);
+  });
 };
